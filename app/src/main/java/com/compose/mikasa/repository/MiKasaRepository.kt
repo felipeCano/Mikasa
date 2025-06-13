3b0587357a2dd14c97a2014af66ea429523ee40c
Luis Felipe Cano A <fcano@carropago.com>
2025-06-12 19:47:26 -0500

    [message]->Removing message

--- app/src/main/java/com/compose/mikasa/repository/MiKasaRepository.kt
+++ app/src/main/java/com/compose/mikasa/repository/MiKasaRepository.kt
@@ -19,7 +19,6 @@ class MiKasaRepository @Inject constructor(
                     emit(ResourceState.Success(it))
                 }
             } else {
-                //webhook pipelines
                 emit(ResourceState.Error("Something went wrong"))
             }
         }
